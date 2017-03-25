c7bf6f46f373b9eb26af23cc1122ab783291f9a8
Benilde <benilde.matimbe@gmail.com>
2017-03-25 12:25:56 +0200

    Updating

--- app/clinic/init.js
+++ app/clinic/init.js
@@ -2,7 +2,11 @@ angular.module('clinic', ['ngRoute', 'ngCookies', 'ui.router', 'smart-table', 'b
                         'bahmni.common.domain', 'bahmni.common.appFramework', 'LocalStorageModule', 'datePicker', 
                         'frapontillo.bootstrap-switch', 'bahmni.common.uicontrols.programmanagment', 'serviceform', 'chart.js',
                         'ui.checkbox', 'application', 'ngMessages', 'poc.common.clinicalservices', 'common.patient',
+<<<<<<< HEAD
                         'pascalprecht.translate', 'bahmni.common.i18n', 'patient.details', 'ui.select2', 'ui.bootstrap']);
+=======
+                        'pascalprecht.translate', 'bahmni.common.i18n', 'patient.details', 'ui.select2', , 'ui.bootstrap']);
+>>>>>>> a67d58f3975a2eda7f72218bf06e8d489b368525
 //to be initialized by config inside app.js
 //needed in dynamic states
 var $stateProviderRef = null;
